9031597726f5503fa547591a6b61305597869290
Paulo Henrique Gonçalves de Oliveira <paulo.oliveira.phgo@gmail.com>
2023-03-31 07:55:22 -0300

    exercicio executado

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Escreva     um     programa     que     verifique     a     validade     de     uma     senha     fornecida pelo     usuário. A     senha     válida     é     o     número     1234. Devem     ser    impressas     as    seguintes     mensagens:ACESSO    PERMITIDO    caso    a    senha    seja    válida.ACESSO    NEGADO    caso    a    senha    seja    inválida.
+
+// Algoritimo 
+// Entrada: senha
+// Proscessamento: 
+
+//Entrada 
+
+Console.WriteLine(@$"
+--------------------------------
+|        Programa para         |
+|      validar uma senha       |
+|                              |
+--------------------------------
+");
+
+Console.WriteLine($"Digite seu senha:");
+int senha = int.Parse(Console.ReadLine());
+
+// Processamento 
+
+
+// Saida
+if (senha==1234)
+{
+Console.WriteLine("ACESSO PERMITIDO");   
+}
+else
+{
+Console.WriteLine("ACESSO NEGADO");
+}
